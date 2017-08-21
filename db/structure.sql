9ac5da1432c77864ad73912083625a9c193853a9
Phillip Medrano <phillip.medrano@gmail.com>
2017-08-21 14:57:51 -0700

    finished search but not styled yet

--- db/structure.sql
+++ db/structure.sql
@@ -266,6 +266,27 @@ ALTER TABLE ONLY users
 
 
 --
+-- Name: customers_lower_email; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX customers_lower_email ON customers USING btree (lower((email)::text));
+
+
+--
+-- Name: customers_lower_first_name; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX customers_lower_first_name ON customers USING btree (lower((first_name)::text) varchar_pattern_ops);
+
+
+--
+-- Name: customers_lower_last_name; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX customers_lower_last_name ON customers USING btree (lower((last_name)::text) varchar_pattern_ops);
+
+
+--
 -- Name: index_customers_on_email; Type: INDEX; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -320,6 +341,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20170619102231'),
 ('20170813235256'),
 ('20170815214239'),
-('20170815230842');
+('20170815230842'),
+('20170821205111');
 
 
